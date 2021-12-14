50f29b51513ae625403f74c9a43dfd079ff0ecd6
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-12-14 17:00:20 +0000

    Added feature tests, added web_helpers.rb, added #find unit-test (#authentication in-progress)

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,9 @@
+feature 'sign-up' do
+  scenario 'User should be able to sign in' do
+    sign_up
+    expect(page).to have_current_path('/sessions/new')
+    expect(page).to have_content("Sign in")
+    
+  end   
+end
+
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,13 +1,27 @@
 require 'user'
 
 describe User do
+  before do
+    @user1 = User.create(email: 'test@test.com', password: 'qwerty', username: 'test')
+  end
   describe '#create' do
     it 'creates a new user' do
-	    user1 = User.create(email: 'test@test.com', password: 'qwerty', username: 'test')
-
-      expect(user1).to be_a User
-      expect(user1.email).to eq 'test@test.com'
+      expect(@user1).to be_a User
+      expect(@user1.email).to eq 'test@test.com'
     end  
+  end
+
+  describe "#find" do
+    it "it finds the user based on the user's id" do
+      result = User.find(id: @user1.id)
+
+      expect(result.id).to eq @user1.id
+      expect(result.email).to eq @user1.email
+    end
   end
 end
 
+describe "#authenticate" do
+  
+
+end
