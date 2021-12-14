fc53c57bc1e6f11805aded6258d3a9d71a732e62
Christopher <dagswebb@gmail.com>
2021-12-14 17:08:50 +0000

    add table spaces to DB && implement into model (wip: needs irb testing)

--- lib/space.rb
+++ lib/space.rb
@@ -1,3 +1,5 @@
+require 'database_connection'
+
 class Space
 
     attr_reader :name, :description, :price_per_night, :available_from, :available_to
@@ -11,6 +13,9 @@ class Space
     end
 
     def self.create(name:, description:, price_per_night:, available_from:, available_to:)
-        Space.new(name: "Makers office", description: "a charming open plan space in E London", price_per_night: "£100", available_from: "01/01/2022", available_to: "06/01/2022")
+        rs = DataConnection.query("INSERT INTO spaces (name, description, price_per_night, available_from, avalaible_to) VALUES($1, $2 , $3, $4, $5, $6)
+          RETURNING id, name, description, price_per_night, available_from, avalaible_to;"
+          [name, description, price_per_night, available_from, avalaible_to])
+        # Space.new(name: "Makers office", description: "a charming open plan space in E London", price_per_night: 100, available_from: "01-01-2022", available_to: "06-01-2022")
     end
 end
--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -3,7 +3,7 @@ require 'space'
 describe Space do
   describe '#create' do
     it 'creates a new space' do
-	    space_1 = Space.create(name: 'Makers Office', description: 'a charming open plan space in E London', price_per_night: '£100', available_from: '01/01/2022', available_to: '06/01/2022')
+	    space_1 = Space.create(name: 'Makers Office', description: 'a charming open plan space in E London', price_per_night: 100, available_from: '01/01/2022', available_to: '06/01/2022')
 
       expect(space_1).to be_a Space
       expect(space_1.description).to eq 'a charming open plan space in E London'
