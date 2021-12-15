a2f7c99225e32045d877845b32e93033fe29fafc
Christopher <dagswebb@gmail.com>
2021-12-15 04:31:03 +0000

    outlines of the next step for user story 2

--- lib/space.rb
+++ lib/space.rb
@@ -5,7 +5,7 @@ class Space
     attr_reader :id, :name, :description, :price_per_night, :available_from, :available_to
 
     def initialize(id:, name:, description:, price_per_night:, available_from:, available_to:)
-        @id = id
+        @id = id          # we decide to have the id just in case we need to display the spaces in a descending order
         @name = name
         @description = description
         @price_per_night = price_per_night
@@ -13,13 +13,28 @@ class Space
         @available_to = available_to
     end
 
+    # def self.all # a method that returns all the spaces listed in the database as a new array
+
+      # 1) result = connect to db and use sql query to select all the rows !!! exept available_from and available_to
+        # we get an array contain hashes? (1 hash/item per row?)
+        # use Time parse + strftime to format our dat if we want (simplest solution?)
+
+      # 2) we .map our result : wrap each data entry into a Space instance
+
+    # end
+
     def self.create(name:, description:, price_per_night:, available_from:, available_to:)
         result = DatabaseConnection.query(
           "INSERT INTO spaces (name, description, price_per_night, available_from, available_to) VALUES($1, $2 , $3, $4, $5)
           RETURNING id, name, description, price_per_night, available_from, available_to;",
           [name, description, price_per_night, available_from, available_to])
 
-          # result = [{id: 'id', name: 'name', description: 'description', price_per_night: 'price_per_night'}]
+        # result = [{id: '1', name: 'Makers office', description: 'a charming open plan space in E London', price_per_night: '100', available_from: '01-01-2022', available_to: '06-01-2022'}]
+
+        # wrapping the result of our sql query into a Space instance (ruby object)
+        # the array only contains 1 hash => index stays at 0
+        # we 'match' the keywords from the #initialize method (parameters)
+        # to the values we get from the result hash (once we do result[0] we are in the array and select from the hash itself)
 
         Space.new(
           id: result[0]['id'],
