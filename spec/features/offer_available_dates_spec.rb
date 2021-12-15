993b72712e56322f8fd0041c871788c911ac4738
EviePalaiochorinou <evangeliapal@gmail.com>
2021-12-15 17:03:11 +0000

    Tests for offering available dates and made a space 1 available dates calendar view

---
+++ spec/features/offer_available_dates_spec.rb
@@ -0,0 +1,11 @@
+feature 'Offering available dates for a space' do
+    scenario 'client can book from available dates' do
+      sign_up
+      sign_in
+      list_a_space
+
+      expect(page).to have_content('Pick a night')
+      expect(page).to have_content('Makers Office')
+      expect(page).to have_button('Request to Book')
+    end
+end
